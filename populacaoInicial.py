380964b21191df91cc2856e3d8fd098ad3233597
Paula Campigotto <paula_campigotto@hotmail.com>
2020-03-04 22:09:32 -0300

    pop

--- populacaoInicial.py
+++ populacaoInicial.py
@@ -1,54 +1,76 @@
+from pprint import pprint
 import random
 import sys
 
-POP = 10
-DIM = 10
+COD = ''
+POP = 0
+DIM = 0
+Li=0
+Ui=0
 
-DOMINIO_INFERIOR_INT = -5
-DOMINIO_SUPERIOR_INT = 10
+def leitura():
+    global COD, POP, DIM, Li, Ui
+    with open('entrada.txt', 'r') as f:
+        linhas = f.readlines()
 
-DOMINIO_INFERIOR_FLOAT = -10
-DOMINIO_SUPERIOR_FLOAT = 10
+    for i in range(len(linhas)):
+        aux = linhas[i].split("\n")
+        linhas[i] = aux[0]
 
-
-def prettyprint(matriz):
-    for i in matriz:
-        print(i)
-
-def binaria():
-    cromossomo = []
-    for i in range(DIM):
-        cromossomo.append(random.randint(0,1))
-    return cromossomo
-
-def inteira():
-    cromossomo = []
-    for i in range(DIM):
-        cromossomo.append(random.randint(DOMINIO_INFERIOR_INT,DOMINIO_SUPERIOR_INT))
-    return cromossomo
-
-def real():
-    cromossomo = []
-    for i in range(DIM):
-        cromossomo.append(round(random.uniform(DOMINIO_INFERIOR_FLOAT, DOMINIO_SUPERIOR_FLOAT), 2))
-    return cromossomo
+    COD = linhas[0].split("=")[1]
+    POP = (int)(linhas[1].split("=")[1])
+    DIM = (int)(linhas[2].split("=")[1])
+    Li = (int)((linhas[3].split("=")[1]).split(",")[0].split("[")[1])
+    Ui = (int)((linhas[3].split("=")[1]).split(",")[1].split("]")[0])
 
 
-populacaoBi = []
-for i in range(POP):
-    populacaoBi.append(binaria())
-prettyprint(populacaoBi)
+def popBinaria():
+    populacao = []
+    for i in range(POP):
+        cromossomo = []
+        for i in range(DIM):
+            cromossomo.append(random.randint(0,1))
+        populacao.append(cromossomo)
+    return populacao
 
-print()
+def popInteira():
+    populacao = []
+    for i in range(POP):
+        cromossomo = []
+        for i in range(DIM):
+            cromossomo.append(random.randint(Li,Ui))
+        populacao.append(cromossomo)
+    return populacao
 
-populacaoInt = []
-for i in range(POP):
-    populacaoInt.append(inteira())
-prettyprint(populacaoInt)
+def popInteiraPerm():
+    populacao = []
+    for i in range(POP):
+        cromossomo = []
+        for i in range(DIM):
+            aux = random.randint(0,DIM-1)
+            while(aux in cromossomo):
+                aux = random.randint(0,DIM-1)
+            cromossomo.append(aux)
+        populacao.append(cromossomo)
+    return populacao
 
-print()
+def popReal():
+    populacao = []
+    for i in range(POP):
+        cromossomo = []
+        for i in range(DIM):
+            cromossomo.append(round(random.uniform(Li, Ui), 2))
+        populacao.append(cromossomo)
+    return populacao
 
-populacaoReal = []
-for i in range(POP):
-    populacaoReal.append(real())
-prettyprint(populacaoReal)
+
+leitura()
+
+if(COD == 'BIN'):
+    pprint(popBinaria())
+elif(COD == 'INT'):
+    pprint(popInteira())
+elif(COD == 'REAL'):
+    pprint(popReal())
+else:
+    pprint(popInteiraPerm())
